03900d286ca16259cbf1783ade5be5c04712b39b
hansb123 <gksajaj@gmail.com>
2026-09-11 18:24:27 +0900

    09_11
    09_11
    Boss Hp bar, Boss Name, Boss Timer 추가완료
    Hud를 수정, 및 보완했습니다.
    대지속성의 스킬 아이콘을 추가했습니다.
    현재 타이틀 씬의 사운드매니저는 사운드를 추가하기만 하면 되도록 만들었습니다.
    아직 레벨업취소 => 스킬포인트 로직은 확인되지 않아 추가하지 않았습니다.

--- Assets/01.Scripts/UI/SoundManager.cs
+++ Assets/01.Scripts/UI/SoundManager.cs
@@ -15,19 +15,27 @@ public enum SFXType
 
 }
 
-public class SoundManager : MonoBehaviour //사운드매니저 풀링 사용할지 아직 결정 X
+public enum SFXUIType
+{
+    Hover,
+    Click
+}
+
+public class SoundManager : MonoBehaviour 
 {
     public static SoundManager instance;
 
-
+    [Header("BGM / SFX / SFX_UI ")]
     [SerializeField] AudioSource bgmAudioSurce;
     [SerializeField] AudioSource sfxAudioSource;
+    [SerializeField] AudioSource sfxUiAudioSource;
 
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider sfxSlider;
 
     public AudioClip[] bgmClip; //배경음
     public AudioClip[] soundClip; //효과음
+    public AudioClip[] soundUiClip; //Ui효과음 
 
     private void Awake()
     {
@@ -43,7 +51,7 @@ public class SoundManager : MonoBehaviour //사운드매니저 풀링 사용할지 아직 결정
 
     void Start()
     {
-        //로컬에 사운드 기록 저장 
+        //로컬에 사운드 기록 저장 (Ui효과음은 조절 x)
         bgmSlider.value = PlayerPrefs.GetFloat("BGM", 0.5f);
         sfxSlider.value = PlayerPrefs.GetFloat("Sfx", 0.5f);
 
@@ -67,6 +75,14 @@ public class SoundManager : MonoBehaviour //사운드매니저 풀링 사용할지 아직 결정
         bgmAudioSurce.Play();
     }
 
+    public void PlaySFXUI(SFXType type)
+    {
+        if ((int)type > soundClip.Length)
+            return;
+
+
+        sfxUiAudioSource.PlayOneShot(soundClip[(int)type]);
+    }
 
 
     public void PlaySFX(SFXType type)
